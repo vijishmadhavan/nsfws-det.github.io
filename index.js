d281a199e2414d05e594c2593d8b23c5acfe1f4d
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2022-10-18 16:23:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
-const NSFWNET_WEIGHTS_PATH ='web_model/model.json';
+const NSFWNET_WEIGHTS_PATH ='model/model.json';
 
 const IMAGE_SIZE = 256;
-const IMAGE_CROP_SIZE = 224;
+const IMAGE_CROP_SIZE = 299;
 const TOPK_PREDICTIONS = 5;
 
 const NSFW_CLASSES = {
   0: 'drawing',
   1: 'hentai',
-  2: 'neural',
+  2: 'neutral',
   3: 'porn',
   4: 'sexy',
 };
@@ -17,7 +16,7 @@ const NSFW_CLASSES = {
 let nsfwnet;
 const nsfwnetDemo = async () => {
 
-  nsfwnet =await tf.loadGraphModel('web_model/model.json');
+  nsfwnet =await tf.loadLayersModel('model/model.json');
 
 
   nsfwnet.predict(tf.zeros([1, IMAGE_CROP_SIZE, IMAGE_CROP_SIZE, 3])).dispose();
@@ -30,8 +29,13 @@ const nsfwnetDemo = async () => {
     predict(image_Element);
     image_Element.style.display = 'block';
   } 
-  document.getElementById('file-container').style.display = 'block';
+  document.getElementById('test_draw').addEventListener('load', () => {
+    predict(image_Element);
+    image_Element.style.display = 'block';
+  });
 };
+
+
 
 
 async function predict(imgElement) {
@@ -40,23 +44,20 @@ async function predict(imgElement) {
   const logits = tf.tidy(() => {
 
     const img = tf.browser.fromPixels(imgElement).toFloat();
-    const crop_image = tf.slice(img, [16, 16, 0], [224, 224, -1]);
-    const img_reshape = tf.reverse(crop_image, [-1]);
-
-    let imagenet_mean = tf.expandDims([103.94, 116.78, 123.68], 0);
-    imagenet_mean = tf.expandDims(imagenet_mean, 0);
-
-    const normalized = img_reshape.sub(imagenet_mean);
-
-    const batched = normalized.reshape([1, IMAGE_CROP_SIZE, IMAGE_CROP_SIZE, 3]);
-
+    const offset = tf.scalar(127.5);
+    const normalized = img.sub(offset).div(offset);
+    const resized = tf.image.resizeBilinear(normalized, [IMAGE_SIZE, IMAGE_SIZE]);
+    const cropped = tf.image.resizeBilinear(resized, [IMAGE_CROP_SIZE, IMAGE_CROP_SIZE]);
+    const batched = cropped.expandDims(0);
     return nsfwnet.predict(batched);
   });
 
   const classes = await getTopKClasses(logits, TOPK_PREDICTIONS);
-
   display(classes);
 }
+
+
+
 
 
 async function getTopKClasses(logits, topK){
